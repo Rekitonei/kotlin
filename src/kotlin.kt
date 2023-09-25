d012c454c85b78f18adc81eae1cf90c4c82af581
Rekitonei <isnainipribadi@gmail.com>
2023-09-25 09:19:09 +0700

    22/09/2023

---
+++ src/kotlin.kt
@@ -0,0 +1,18 @@
+fun main (){
+    //type string
+    val myName = "isnaini"
+    //type int
+    var age = 17
+    print("Hello" + myName)
+
+    //Integer TYPES: Byte (8bit), Short (16 bit),
+    //Int (32 bit), Long (64 bit)
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int =123123123
+    val myLong: Long = 39_812_309_487_120_300
+
+    // Floating Point number types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979323846
+}
